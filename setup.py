ede71b195296db3e573b7bc73e19216f96ed0f59
Robin Dupont <robin.dpnt@gmail.com>
2021-10-25 13:54:46 +0200

    update version + requirements in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pykefcontrol',         
   packages = ['pykefcontrol'],  
-  version = '0.3',     
+  version = '0.4',     
   license='MIT',        
   description = 'Python library for controling the KEF LS50 Wireless II',   
   long_description= 'Python library for controling the KEF LS50 Wireless II. It supports basic commands for setting the volume, the source, and getting the media playing informations',
@@ -11,7 +11,8 @@ setup(
   url = 'https://github.com/rodupont/pykefcontrol',   
   keywords = ['Kef', 'Speaker', 'Wireless II', 'Wireless 2'],   
   install_requires=[          
-          'requests>=2.26.0',
+          'requests==2.26.0',
+          'aiohttp==3.7.4'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
