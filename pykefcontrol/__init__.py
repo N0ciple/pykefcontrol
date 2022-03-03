1c4b17770f0b525f4c94ea769b57524aa5bbfc25
Robin Dupont <robin.dpnt@gmail.com>
2022-03-03 15:31:14 +0100

    Unpin and update dependencies (Needed to avoid dependencies conflicts because home assistant is using the new pip resolver)

--- pykefcontrol/__init__.py
+++ pykefcontrol/__init__.py
@@ -1,3 +1,3 @@
 from pykefcontrol.kef_connector import KefConnector, KefAsyncConnector
 
-__version__ = 0.5
+__version__ = "0.5.1"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pykefcontrol',         
   packages = ['pykefcontrol'],  
-  version = '0.5',     
+  version = '0.5.1',     
   license='MIT',        
   description = 'Python library for controling the KEF LS50 Wireless II',   
   long_description= 'Python library for controling the KEF LS50 Wireless II. It supports basic commands for setting the volume, the source, and getting the media playing informations',
@@ -11,11 +11,11 @@ setup(
   url = 'https://github.com/rodupont/pykefcontrol',   
   keywords = ['Kef', 'Speaker', 'Wireless II', 'Wireless 2'],   
   install_requires=[          
-          'requests==2.26.0',
-          'aiohttp==3.7.4'
+          'requests>=2.26.0',
+          'aiohttp>=3.7.4'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Topic :: Home Automation',
     'Programming Language :: Python :: 3',     
     'Programming Language :: Python :: 3.4',
@@ -24,5 +24,6 @@ setup(
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
   ],
 )
